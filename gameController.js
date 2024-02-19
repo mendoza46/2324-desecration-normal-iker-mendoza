1783426950d45f3d2c85c9195521eab928f146c4
mendoza46 <iker.mendoza@ikasle.aeg.eus>
2024-02-19 15:20:26 +0100

    [NEW] crear objeto con el heroe random

--- gameController.js
+++ gameController.js
@@ -1,12 +1,26 @@
 const game = async (heroes) => {
     console.log("entra en la fncion game")
     const villainZarate = await getZarate(heroes);
+
+    const superHero = await getRandomHero(heroes, villainZarate);
 }
 
 const getZarate = async (heroes) => {
-    const villainZarate = heroes.find(character => character.name === "Junkpile");
+    const villainZarate = heroes.find(hero => hero.name === "Junkpile");
     // console.log(zarate);
     return villainZarate;
+}
+
+const getRandomHero = async (heroes, villainZarate) => {
+
+    let randomNum;
+    
+    do{
+        randomNum = Math.floor(Math.random() * heroes.length);
+    } while (randomNum === villainZarate.id);
+
+    const superHero = heroes.find(hero => hero.id === randomNum);
+    return superHero;
 }
 
 module.exports = {
